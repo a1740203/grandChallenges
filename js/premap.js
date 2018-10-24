30fe5efc120481ae1ece56ef97e9d75761a9db01
Rory <rorymartin98@gmail.com>
2018-10-25 02:57:10 +1030

    updateNight

--- js/premap.js
+++ js/premap.js
@@ -1,11 +1,14 @@
 /* Scripts  */
-
-<!--
-    initialState1();
-    initialState3();
-    uploadState();
-    sliderStore();
+    var fileName = "0", map = "0", matchID = 0, time = -1200, winner = "0", initTmp = 0, divState = 0;; // Global Variables
     
+    function initialize() {
+        initialState1();
+        initialState2();
+        initialState3();
+        initialState3h();
+        sliderStore("0"); // Temp value on initiate
+        document.getElementById("heatmapContainerWrapper2").style.display = "none"; // Combined div initiate
+    }
     function initialState1() {
         document.getElementById("heatmapContainerInit").style.display = "none";
         document.getElementById("heatmapContainer1").style.display = "none";
@@ -14,11 +17,6 @@
         document.getElementById("heatmapContainer4").style.display = "none";
         document.getElementById("heatmapContainer4Cover").style.display = "none";
         document.getElementById("heatmapContainer5").style.display = "none";
-        document.getElementById("heatmapUpload0").style.display = "none";
-        document.getElementById("heatmapUpload1").style.display = "none";
-        document.getElementById("heatmapUpload2").style.display = "none";
-        document.getElementById("heatmapUpload3").style.display = "none";
-        document.getElementById("heatmapUpload4").style.display = "none";
         document.getElementById("text01").style.display = "none";
         document.getElementById("text02").style.display = "none";
         document.getElementById("text03").style.display = "none";
@@ -27,14 +25,21 @@
         document.getElementById("text3").style.display = "none";
         document.getElementById("text4").style.display = "none";
         document.getElementById("text5").style.display = "none";
-        document.getElementById("text301").style.display = "none";
-        document.getElementById("text311").style.display = "none";
-        document.getElementById("text321").style.display = "none";
-        document.getElementById("text331").style.display = "none";
-        document.getElementById("text302").style.display = "none";
-        document.getElementById("text312").style.display = "none";
-        document.getElementById("text322").style.display = "none";
-        document.getElementById("text332").style.display = "none";
+    }
+    function initialState2() {
+        document.getElementById("heatmapContainerInitc").style.display = "none";
+        document.getElementById("heatmapContainer1c").style.display = "none";
+        document.getElementById("heatmapContainer2c").style.display = "none";
+        document.getElementById("heatmapContainer3c").style.display = "none";
+        document.getElementById("heatmapContainer4c").style.display = "none";
+        document.getElementById("heatmapContainer4Coverc").style.display = "none";
+        document.getElementById("heatmapContainer5c").style.display = "none";
+        document.getElementById("heatmapCombinedInit").style.display = "none";
+        document.getElementById("heatmapCombined1").style.display = "none";
+        document.getElementById("heatmapCombined2").style.display = "none";
+        document.getElementById("heatmapCombined3").style.display = "none";
+        document.getElementById("heatmapCombined4").style.display = "none";
+        document.getElementById("heatmapCombined5").style.display = "none"; 
     }
     function initialState3() {
         document.getElementById("text301").style.display = "none";
@@ -48,30 +53,34 @@
         document.getElementById("text3000").style.display = "none";
         document.getElementById("text3001").style.display = "none";
         document.getElementById("text3002").style.display = "none";
-    }
-    function uploadState() {
-        initialState3();
-        document.getElementById("text301").style.display = "block";
-        document.getElementById("text302").style.display = "block";
-        document.getElementById("heatmapUpload0").style.display = "none";
-        document.getElementById("heatmapUpload1").style.display = "none";
-        document.getElementById("heatmapUpload2").style.display = "none";
-        document.getElementById("heatmapUpload3").style.display = "none";
-        document.getElementById("heatmapUpload4").style.display = "none";
         document.getElementById("map").innerHTML = map;
         document.getElementById("matchID").innerHTML = matchID;
         document.getElementById("timeEnd").innerHTML = parseInt(20+(time/60))+"minutes";
         document.getElementById("winner").innerHTML = winner;
     }
+    function initialState3h() {
+        document.getElementById("heatmapUpload0").style.display = "none";
+        document.getElementById("heatmapUpload1").style.display = "none";
+        document.getElementById("heatmapUpload2").style.display = "none";
+        document.getElementById("heatmapUpload3").style.display = "none";
+        document.getElementById("heatmapUpload4").style.display = "none";
+    }
     
     function sliderStore(temp) {
-        var tmp = temp;
+        var tmp1, tmp2, tmp3;
+        if (initTmp == 0) {
+            tmp1 = temp;
+            tmp2 = temp;
+            tmp3 = temp;
+            initTmp = 1;
+        }
         var sliderState1 = document.getElementById("myRange1").value;
         var sliderState2 = document.getElementById("myRange2").value;
         var sliderState3 = document.getElementById("myRange3").value;
         
+        // Slider 1
         switch (true) {
-            case (sliderState1 == 0), (tmp == 0):
+            case ((sliderState1 == 0) || (tmp1 == 0)):
                 initialState1();
                 tmp = 1;
                 document.getElementById("text01").style.display = "block";
@@ -114,7 +123,81 @@
                 document.getElementById("heatmapContainer4Cover").style.display = "block";
                 document.getElementById("heatmapContainer4Cover").style.opacity = "1";
                 break;
-            case (sliderState3 == 0):
+        }
+        
+        // Slider 2
+        switch (true) {
+            case ((sliderState2 == 0) || (tmp2 == 0)):
+                initialState2();
+                tmp2 = 1;
+                document.getElementById("text201").style.display = "block";
+                document.getElementById("text202").style.display = "block";
+                document.getElementById("text203").style.display = "block";
+                document.getElementById("heatmapContainerInitc").style.display = "block";
+                document.getElementById("heatmapContainerInitc").style.opacity = "1";
+                document.getElementById("heatmapCombinedInit").style.display = "block";
+                document.getElementById("heatmapCombinedInit").style.opacity = "0.5";
+                break;
+            case (sliderState2 == 1):
+                initialState2();
+                //document.getElementById("text1c").style.display = "block";
+                document.getElementById("heatmapContainer1c").style.display = "block";
+                document.getElementById("heatmapContainer1c").style.opacity = "1";
+                document.getElementById("heatmapCombined1").style.display = "block";
+                document.getElementById("heatmapCombined1").style.opacity = "0.5";
+                break;
+            case (sliderState2 == 2):
+                initialState2();
+                //document.getElementById("text2c").style.display = "block";
+                document.getElementById("heatmapContainer2c").style.display = "block";
+                document.getElementById("heatmapContainer2c").style.opacity = "1";
+                document.getElementById("heatmapCombined2").style.display = "block";
+                document.getElementById("heatmapCombined2").style.opacity = "0.5";
+                break;
+            case (sliderState2 == 3):
+                initialState2();
+                //document.getElementById("text3c").style.display = "block";
+                document.getElementById("heatmapContainer3c").style.display = "block";
+                document.getElementById("heatmapContainer3c").style.opacity = "1";
+                document.getElementById("heatmapCombined3").style.display = "block";
+                document.getElementById("heatmapCombined3").style.opacity = "0.5";
+                break;
+            case (sliderState2 == 4):
+                initialState2();
+                //document.getElementById("text4c").style.display = "block";
+                document.getElementById("heatmapContainer4c").style.display = "block";
+                document.getElementById("heatmapContainer4c").style.opacity = "1";
+                document.getElementById("heatmapContainer4Coverc").style.display = "block";
+                document.getElementById("heatmapContainer4Coverc").style.opacity = "1";
+                document.getElementById("heatmapCombined4").style.display = "block";
+                document.getElementById("heatmapCombined4").style.opacity = "0.5";
+                break;
+            case (sliderState2 == 5):
+                initialState2();
+                //document.getElementById("text5c").style.display = "block";
+                document.getElementById("heatmapContainer5c").style.display = "block";
+                document.getElementById("heatmapContainer5c").style.opacity = "1";
+                document.getElementById("heatmapContainer4Coverc").style.display = "block";
+                document.getElementById("heatmapContainer4Coverc").style.opacity = "1";
+                document.getElementById("heatmapCombined5").style.display = "block";
+                document.getElementById("heatmapCombined5").style.opacity = "0.5";
+                break;
+        }
+        
+        // Slider 3
+        if ((fileName == "0") && (initTmp == 1)) {
+            sliderState3 = 0;
+            tmp3 = 0;
+        }
+        switch (true) {
+            case ((sliderState3 == 0) && (tmp3 == 0)):
+                initialState3();
+                tmp3 = 1;
+                document.getElementById("text3000").style.display = "block";
+                document.getElementById("text3001").style.display = "block";
+                document.getElementById("text3002").style.display = "block";
+                break;
+            case ((sliderState3 == 0) || (tmp3 == 1)):
                 initialState3();
                 document.getElementById("text301").style.display = "block";
                 document.getElementById("text302").style.display = "block";
@@ -141,49 +224,61 @@
         document.getElementById("myRange3").value = 0;
         switch(fileName) {
             default:
-                uploadState();
-                initialState3();
-                document.getElementById("map").innerHTML = "";
-                document.getElementById("matchID").innerHTML = "";
-                document.getElementById("timeEnd").innerHTML = "";
-                document.getElementById("winner").innerHTML = "";
+                initialState3h();
                 document.getElementById("text3000").style.display = "block";
                 document.getElementById("text3001").style.display = "block";
                 document.getElementById("text3002").style.display = "block";
                 alert("File uploaded not recognized as PUBG csv file.");
                 break;
             case "testUploadZero.csv":
-                uploadState();
+                initialState3h();
                 document.getElementById("heatmapUpload0").style.display = "block";
                 document.getElementById("heatmapUpload0").style.opacity = "1";
+                alert("Upload successful.");
                 break;
             case "testUploadOne.csv":
-                uploadState();
+                initialState3h();
                 document.getElementById("heatmapUpload1").style.display = "block";
                 document.getElementById("heatmapUpload1").style.opacity = "1";
+                alert("Upload successful.");
                 break;
             case "testUploadTwo.csv":
-                uploadState();
+                initialState3h();
                 document.getElementById("heatmapUpload2").style.display = "block";
                 document.getElementById("heatmapUpload2").style.opacity = "1";
+                alert("Upload successful.");
                 break;
             case "testUploadThree.csv":
-                uploadState();
+                initialState3h();
                 document.getElementById("heatmapUpload3").style.display = "block";
                 document.getElementById("heatmapUpload3").style.opacity = "1";
+                alert("Upload successful.");
                 break;
             case "testUploadFour.csv":
-                uploadState();
+                initialState3h();
                 document.getElementById("heatmapUpload4").style.display = "block";
                 document.getElementById("heatmapUpload4").style.opacity = "1";
+                alert("Upload successful.");
                 break;
         }
     }
     
+    function hideShow() {
+        if (divState == 0) {
+            document.getElementById("heatmapContainerWrapper2").style.display = "block";
+            divState = 1;
+            return;
+        }
+        else if (divState == 1) {
+            document.getElementById("heatmapContainerWrapper2").style.display = "none";
+            divState = 0;
+            return;
+        } 
+    }
+    
     function handleFiles(files) {
-        var filePath = document.getElementById("csvFileInput").value;
+        var filePath = document.getElementById("csvFileInput3").value;
         fileName = filePath.substring(filePath.lastIndexOf("\\")+1);
-        
         // Check for the various File API support.
         if (window.FileReader) {
             // FileReader are supported.
@@ -231,45 +326,53 @@
         // 0123456 7890123
         // 1209 526111 8 432951 8
         // 0123 456789 0 123456 7
-        
-        for (i = 11; i < 12 ; i++) {
-            map = "Miramar";
-            matchID = res[i].slice(0, 9);
-            time = res[i].slice(9, 12);
-            winner = res[i].slice(17, 19);      
-            /* Needed for heatmap creatability
-                var xcoordconv = res[i].slice(13, 18);
-                var xcoord = parseInt((((xcoordconv/800000)*500)));
-                var ycoordconv = res[i].slice(20, 26)*4;
-                var ycoord = parseInt((((ycoordconv/800000)*500)));
-                var strength = 1;   
-            if (((xcoord || ycoord) <= 0) || ((xcoord || ycoord) >= 500)) { //Skips bluezone kills
-                continue;
+        if (fileName.includes("test") == true) {
+            for (i = 11; i < 12 ; i++) {
+                map = "Miramar";
+                matchID = res[i].slice(0, 9);
+                time = res[i].slice(9, 12);
+                winner = res[i].slice(17, 19);      
+                /* Needed for heatmap creatability
+                    var xcoordconv = res[i].slice(13, 18);
+                    var xcoord = parseInt((((xcoordconv/800000)*500)));
+                    var ycoordconv = res[i].slice(20, 26)*4;
+                    var ycoord = parseInt((((ycoordconv/800000)*500)));
+                    var strength = 1;   
+                if (((xcoord || ycoord) <= 0) || ((xcoord || ycoord) >= 500)) { //Skips bluezone kills
+                    continue;
+                }
+                //h.addData({x: xcoord, y: ycoord, value: strength });
+                //alert("x-coordinate is: "+xcoord+" which is: "+xcoordconv+"\n y-coordinate is: "+ycoord+" which is: "+ycoordconv);
+                h.addData({x: 1000, y: 70*i, value: strength });
+                
+                if (time == 0 && greenLight == 0) { // 0 minutes
+                }
+                if (time <= 240 && time >= 0 && sliderState == 1) { // 0 to 4 minutes
+                    h.addData({x: xcoord, y: ycoord, value: strength });
+                }
+                if (time <= 480 && time > 240 && sliderState == 2) { // 4 to 8 minutes
+                    h.addData({x: xcoord, y: ycoord, value: strength });
+                }
+                if (time <= 720 && time > 480 && sliderState == 3) { // 8 to 12 minutes
+                    h.addData({x: xcoord, y: ycoord, value: strength });
+                }
+                if (time <= 960 && time > 720 && sliderState == 4) { // 12 to 16 minutes
+                    h.addData({x: xcoord, y: ycoord, value: strength });
+                }
+                if (time <= 1200 && time > 960 && sliderState == 5) { // 16 to 20 minutes
+                    h.addData({x: xcoord, y: ycoord, value: strength });
+                }
+                */
             }
-            //h.addData({x: xcoord, y: ycoord, value: strength });
-            //alert("x-coordinate is: "+xcoord+" which is: "+xcoordconv+"\n y-coordinate is: "+ycoord+" which is: "+ycoordconv);
-            h.addData({x: 1000, y: 70*i, value: strength });
-            
-            if (time == 0 && greenLight == 0) { // 0 minutes
-            }
-            if (time <= 240 && time >= 0 && sliderState == 1) { // 0 to 4 minutes
-                h.addData({x: xcoord, y: ycoord, value: strength });
-            }
-            if (time <= 480 && time > 240 && sliderState == 2) { // 4 to 8 minutes
-                h.addData({x: xcoord, y: ycoord, value: strength });
-            }
-            if (time <= 720 && time > 480 && sliderState == 3) { // 8 to 12 minutes
-                h.addData({x: xcoord, y: ycoord, value: strength });
-            }
-            if (time <= 960 && time > 720 && sliderState == 4) { // 12 to 16 minutes
-                h.addData({x: xcoord, y: ycoord, value: strength });
-            }
-            if (time <= 1200 && time > 960 && sliderState == 5) { // 16 to 20 minutes
-                h.addData({x: xcoord, y: ycoord, value: strength });
-            }
-            */   
+            sliderStore();fileNameParse();
         }
-        fileNameParse();
+        else {
+            initialState3();
+            document.getElementById("text3000").style.display = "block";
+            document.getElementById("text3001").style.display = "block";
+            document.getElementById("text3002").style.display = "block";
+            fileName = "0";
+            initTmp = 1;
+            sliderStore("0");fileNameParse();
+        }
     }
-    
--->
--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,21 @@
 /* Scripts  */
 
-<!--
-
     function scrollFunction() {
-        var fileName, map, matchID, time, winner;
-        /*
-        // 240248990 1209 5261118 4329518 70
-        // 012345678 9012 3456789 0123456 7
-            
-            for (var i = 0; i < 1000; i++) { // Test values: x = 657725.1 y = 146275.2
-                var matchID = res[i].slice(0, 8);
-                var time = res[i].slice(9, 12);
-                var winner = res[i].slice(17, 18);
-                var xcoordconv = res[i].slice(13, 18);
-                var xcoord = parseInt((((xcoordconv/800000)*500)));
-                var ycoordconv = res[i].slice(20, 26)*4;
-                var ycoord = parseInt((((ycoordconv/800000)*500)));
-                var strength = 1;
-        */ 
-        
-        sliderStore("0");
         window.onbeforeunload = function () {
-        window.scrollTo(0,0);
-        
-        window.onscroll = function() {
-            headerFloat();
-        }
-        
-        var header = document.getElementById("h_Container");
-        var sticky = header.offsetTop;
-
-        function headerFloat() {
-            if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-            }
-            else {
-                header.classList.remove("sticky");
-            }
-        }
+            window.scrollTo(0,0);
+                window.onscroll = function() {
+                    headerFloat();
+                }
+            var header = document.getElementById("h_Container");
+            var sticky = header.offsetTop;
+                function headerFloat() {
+                    if (window.pageYOffset > sticky) {
+                    header.classList.add("sticky");
+                    }
+                    else {
+                        header.classList.remove("sticky");
+                    }
+                }
         }
         $(document).ready(function() {
             // Adds smooth scrolling to a links in css
@@ -62,4 +40,3 @@
             });
         });
     }
--->
